f7030a770e52cdefc28c1d4b6bec21ae4fe7414f
TedMANNERs <lukas-braegger@hotmail.com>
2015-01-08 14:24:26 +0100

    Replaced server world with a 2d-array.

--- BusyBeetle.Server/ServerService.cs
+++ BusyBeetle.Server/ServerService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Net.Sockets;
@@ -11,21 +12,23 @@ namespace BusyBeetle.Server
 {
     public class ServerService : IService, IDisposable
     {
+        private const int Height = 200;
+        private const int Width = 200;
         private readonly object _lockObject = new object();
         private readonly List<PixelData> _modifiedPixels = new List<PixelData>();
         private readonly ISerializer _serializer;
-        private readonly IWorld _world;
         private Task _clientHandler;
         private IList<Client> _clients;
         private IConfiguration _config;
         private Task _connectionHandler;
         private bool _isRunning;
         private TcpListener _listener;
+        private Color[][] _world;
 
         public ServerService(ISerializer serializer, IWorldFactory worldFactory)
         {
             _serializer = serializer;
-            _world = worldFactory.Create(200, 200, false);
+            InitPixelArray(Width, Height);
             _clients = new List<Client>();
         }
 
@@ -61,6 +64,20 @@ namespace BusyBeetle.Server
             _clientHandler.Wait();
         }
 
+        private void InitPixelArray(int width, int height)
+        {
+            _world = new Color[width][];
+
+            for (int i = 0; i < width; i++)
+            {
+                _world[i] = new Color[height];
+                for (int j = 0; j < height; j++)
+                {
+                    _world[i][j] = Color.White;
+                }
+            }
+        }
+
         protected virtual void Dispose(bool disposing)
         {
             if (disposing)
@@ -88,15 +105,15 @@ namespace BusyBeetle.Server
                         NetworkStream stream = tcpClient.GetStream();
                         List<PixelData> pixels = new List<PixelData>();
 
-                        for (int i = 0; i < _world.Width; i++)
+                        for (int i = 0; i < Width; i++)
                         {
-                            for (int j = 0; j < _world.Height; j++)
+                            for (int j = 0; j < Height; j++)
                             {
-                                pixels.Add(new PixelData(i, j, _world.GetAt(i, j)));
+                                pixels.Add(new PixelData(i, j, _world[i][j]));
                             }
                         }
 
-                        int[] worldSize = { _world.Width, _world.Height };
+                        int[] worldSize = { Width, Height };
                         byte[] sizeBytes = _serializer.Serialize(new Packet { Type = PacketType.SizeNegotiation, Content = worldSize });
                         stream.Write(sizeBytes, 0, sizeBytes.Length);
                         stream.Flush();
@@ -147,7 +164,7 @@ namespace BusyBeetle.Server
                                 {
                                     _modifiedPixels.Add(pixel);
                                     client.LastModifiedPixels.Add(pixel);
-                                    _world.Bitmap.SetPixel(pixel.PositionX, pixel.PositionY, pixel.Color);
+                                    _world[pixel.PositionX][pixel.PositionY] = pixel.Color;
                                 }
                             }
                             catch (IOException e)
