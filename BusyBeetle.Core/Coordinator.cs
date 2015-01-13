9a0c0093bc180154c10987f088d6262baa7a6b3d
TedMANNERs <lukas-braegger@hotmail.com>
2015-01-13 13:21:59 +0100

    Forgot Coordinator :/

--- BusyBeetle.Core/Coordinator.cs
+++ BusyBeetle.Core/Coordinator.cs
@@ -1,24 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Drawing;
+﻿using System.ComponentModel;
 using System.Runtime.CompilerServices;
-using System.Threading.Tasks;
 using BusyBeetle.Core.Properties;
-using Point = System.Windows.Point;
 
 namespace BusyBeetle.Core
 {
     public class Coordinator : ICoordinator, INotifyPropertyChanged
     {
         private IWorld _world;
-
-        public Coordinator()
-        {
-            BeetleTasks = new List<Task>();
-        }
-
-        public IList<Task> BeetleTasks { get; set; }
 
         public IWorld World
         {
@@ -27,42 +15,6 @@ namespace BusyBeetle.Core
             {
                 _world = value;
                 OnPropertyChanged();
-            }
-        }
-
-        public void SpawnAt(Point position, System.Windows.Media.Color color)
-        {
-            if (World.GameType == GameType.GameOfLife)
-            {
-                Random r = new Random();
-
-                for (int i = 0; i < 5; i++)
-                {
-                    for (int j = 0; j < 5; j++)
-                    {
-                        if (r.Next(2) == 0)
-                            World.SetAt((int)(position.X / Values.Scalefactor) + i, (int)(position.Y / Values.Scalefactor) + j, Color.Black);
-                    }
-                }
-            }
-            else
-            {
-                Task task = new Task(
-                    () =>
-                    {
-                        lock (World.Beetles)
-                        {
-                            Beetle beetle = new Beetle((int)(position.X / Values.Scalefactor), (int)(position.Y / Values.Scalefactor), Color.FromArgb(color.A, color.R, color.G, color.B));
-                            World.Beetles.Add(beetle);
-
-                            //for (int i = 0; i < World.Width; i += 2)
-                            //{
-                            //    World.Beetles.Add(new Beetle(i, 50, Color.FromArgb(i * 5 % 255, i * 5 % 255, i * 5 % 255)));
-                            //}
-                        }
-                    });
-                BeetleTasks.Add(task);
-                task.Start();
             }
         }
 
--- BusyBeetle.Core/ICoordinator.cs
+++ BusyBeetle.Core/ICoordinator.cs
@@ -1,14 +1,7 @@
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using System.Windows;
-
 namespace BusyBeetle.Core
 {
     public interface ICoordinator
     {
         IWorld World { get; set; }
-        IList<Task> BeetleTasks { get; set; }
-
-        void SpawnAt(Point position, System.Windows.Media.Color color);
     }
 }
