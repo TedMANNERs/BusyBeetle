2b3366c988710ba726a241b4d9f0bb5abca69b62
TedMANNERs <lukas-braegger@hotmail.com>
2015-01-20 16:48:45 +0100

    Added additional Serialization tests

--- BusyBeetle.CoreTest/Serialization/SerializerTest.cs
+++ BusyBeetle.CoreTest/Serialization/SerializerTest.cs
@@ -19,6 +19,7 @@ namespace BusyBeetle.CoreTest.Serialization
         }
 
         private Serializer _testee;
+        private const int PixelDataSize = 5;
 
         [Test]
         public void Deserialize_WhenStreamContainOnePixelData_ThenReturnPixelDataList()
@@ -65,6 +66,44 @@ namespace BusyBeetle.CoreTest.Serialization
         }
 
         [Test]
+        public void Deserialize_WhenStreamContains300PixelData_ThenReturnPixelDataList()
+        {
+            // arrange
+            const int PixelCount = 300;
+            const int ByteCount = PixelCount * PixelDataSize + 5;
+            byte[] bytes = new byte[ByteCount];
+            bytes[0] = (byte)PacketType.PixelData;
+            bytes[1] = (0xFe);
+            bytes[2] = PixelCount & 0xFF;
+            bytes[3] = (PixelCount >> 8) & 0xFF;
+            bytes[4] = (PixelCount >> 16) & 0xFF;
+
+            int offset = PixelDataSize;
+            for (int i = 0; i < PixelCount; i++)
+            {
+                bytes[offset] = 50;
+                bytes[offset + 1] = 50;
+                bytes[offset + 2] = 0x33;
+                bytes[offset + 3] = 0x33;
+                bytes[offset + 4] = 0xFF;
+                offset += PixelDataSize;
+            }
+
+            MemoryStream stream = new MemoryStream();
+            stream.Write(bytes, 0, bytes.Length);
+            stream.Flush();
+            stream.Position = 0;
+
+            // act
+            List<PixelData> pixels = (List<PixelData>)_testee.Deserialize(stream).Content;
+            stream.Close();
+
+            // assert
+            for (int i = 0; i < PixelCount; i++)
+                pixels[i].ShouldBeEquivalentTo(new PixelData(50, 50, Color.FromArgb(0x33, 0x33, 0xFF)));
+        }
+
+        [Test]
         public void Deserialize_WhenStreamContainsSizeNegotiation_ThenReturnSize()
         {
             // arrange
@@ -81,6 +120,44 @@ namespace BusyBeetle.CoreTest.Serialization
 
             // assert
             size.ShouldAllBeEquivalentTo(expectedSize);
+        }
+
+        [Test]
+        public void Serialize_WhenInputContains300PixelData_ThenReturnBytes()
+        {
+            // arrange
+            const int PixelCount = 300;
+            const int ByteCount = PixelCount * PixelDataSize + 5;
+
+            List<PixelData> pixels = new List<PixelData>();
+            for (int i = 0; i < PixelCount; i++)
+            {
+                pixels.Add(new PixelData(50, 50, Color.Red));
+            }
+
+            int offset = PixelDataSize;
+            byte[] expectedBytes = new byte[ByteCount];
+            expectedBytes[0] = (byte)PacketType.PixelData;
+            expectedBytes[1] = (0xFe);
+            expectedBytes[2] = PixelCount & 0xFF;
+            expectedBytes[3] = (PixelCount >> 8) & 0xFF;
+            expectedBytes[4] = (PixelCount >> 16) & 0xFF;
+
+            for (int i = 0; i < PixelCount; i++)
+            {
+                expectedBytes[offset] = 50;
+                expectedBytes[offset + 1] = 50;
+                expectedBytes[offset + 2] = 0xFF;
+                expectedBytes[offset + 3] = 0x00;
+                expectedBytes[offset + 4] = 0x00;
+                offset += PixelDataSize;
+            }
+
+            // act
+            byte[] bytes = _testee.Serialize(new Packet { Type = PacketType.PixelData, Content = pixels });
+
+            // assert
+            bytes.ShouldAllBeEquivalentTo(expectedBytes);
         }
 
         [Test]
